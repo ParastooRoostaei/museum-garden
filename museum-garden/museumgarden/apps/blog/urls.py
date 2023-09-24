516edea132b5ac8944745cbfa84841ae76134c42
ParastooRoostaei <parastooRoostaei@yahoo.com>
2023-09-24 19:12:15 +0200

    museum-garden

---
+++ museum-garden/museumgarden/apps/blog/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+import apps.blog.views as views
+urlpatterns= [
+        path('',views.blog),          
+]
---
+++ museum-garden/museumgarden/apps/main/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+import apps.main.views as views
+urlpatterns= [
+          path('', views.index), 
+]
